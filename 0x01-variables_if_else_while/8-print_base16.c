46c2a0c4e86406fef88b19783f01c064324af684
Your Name <ismailillias34@gmail.com>
2023-10-08 02:21:15 -0700

    n

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main- print base 16 numbers
+ *
+ * Return: Always (0) success
+ */
+
+int main(void)
+{	
+	/* Print all the numbers in base 16 */
+	
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		putchar('0' + ch);
+	}
+
+	for (ch = 'a'; ch <= 'f'; ch ++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
