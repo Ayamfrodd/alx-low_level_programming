4764ea5b98ca4eaf4f63a2ddb1ac63e2e12005cb
Your Name <ismailillias34@gmail.com>
2023-10-08 02:39:01 -0700

    g

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- print numberts
+ *
+ * Return: Always (0) success
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(48 + i);
+		putchar(44);
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
