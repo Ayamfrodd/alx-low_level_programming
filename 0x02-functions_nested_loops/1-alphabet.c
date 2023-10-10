3bb9421109cb98f8e4bad962ad3ddd8c8a8ba7d1
Your Name <ismailillias34@gmail.com>
2023-10-10 10:21:15 -0700

    added new file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void print_alphabet(void);
+
+/**
+ * print_alphabet - print all the alphabets
+ */
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+}
+
+/**
+ * main - print alphabet
+ *
+ * Return : Alwyas (0) success
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
