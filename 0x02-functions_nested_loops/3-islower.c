f6dad75bbba7eceebe00aa1911315ddbfb3eaca9
Your Name <ismailillias34@gmail.com>
2023-10-10 11:58:43 -0700

    new

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
+/**
+ * _islower - check if value is lower
+ *
+ */
+
 int _islower(int c)
 {
-	if (c>='a' && c<= 'z')
+	if (c >= 'a' && c <= 'z')
 	return (1);
-	return(0);
+	return (0);
 }
