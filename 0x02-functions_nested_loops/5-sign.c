eede7470468ba85569a707a1ae6c3d8fbb122c08
Your Name <ismailillias34@gmail.com>
2023-10-10 14:24:19 -0700

    g

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign- print signs
+ * Return: return 1 if positive, -1 if negative and Alyas (0) success
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+	putchar (48);
+	return (0);
+}
