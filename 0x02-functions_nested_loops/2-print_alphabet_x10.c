8c803fedbc85a9dcf030d82e46761aa49362bf1e
Your Name <ismailillias34@gmail.com>
2023-10-10 11:37:25 -0700

    new

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,11 +11,10 @@ void print_alphabet_x10(void)
 	
 	for(i=0; i<10;i++)
 	{
-		while(ch <= 'z')
+		for(ch='a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
