78a7669865dc3c45122afa559dfeb2da9a897e42
Shepherd <ismailillias34@gmail.com>
2023-10-13 09:31:14 -0700

    new

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/** 
+ * _isupper - check if int is caps
+ *
+ * @c - int to check
+ *
+ * Return : 1 if caps and 0 if not
+ */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
