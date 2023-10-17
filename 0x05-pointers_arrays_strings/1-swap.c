f62f6f9b6a48d4a07eaf8aa37be7641c6dba8e93
Shepherd <ismailillias34@gmail.com>
2023-10-17 09:49:54 -0700

    Added

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int : swap int a and b
+ * @a : first int;
+ * @b : second int;
+ * Return : Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *c;
+	
+	*a = *c;
+	*a = *b;
+	*b = *c;
+}
